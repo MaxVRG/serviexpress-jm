386f56273f73b08102cc467de2e133900e03b60f
MaxVRG <max.vargasp@duocuc.cl>
2024-11-11 18:52:44 -0300

    V1.2
    Home
    Servicios

--- ServiAPP/coreapp/views.py
+++ ServiAPP/coreapp/views.py
@@ -2,3 +2,7 @@ from django.shortcuts import render
 
 def home(request):
     return render(request, 'coreapp/home.html')
+
+
+def servicios(request):
+    return render(request, 'coreapp/servicios.html')
